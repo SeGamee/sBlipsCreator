3aef4fee213d491faaf37f894df63007a56f8beb
SeGame <48103724+SeGamee@users.noreply.github.com>
2023-04-25 20:12:49 +0200

    Update client.lua

--- sBlipsCreator/client.lua
+++ sBlipsCreator/client.lua
@@ -1,5 +1,5 @@
 ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 function KeyboardInput(TextEntry, ExampleText, MaxStringLenght)
 	AddTextEntry('FMMC_KEY_TIP1', TextEntry)
